7098dcb933a44e0920a5b90100b52d746ea515e0
Ioannis Bekiaris <ioannis@blendo.co>
2017-02-02 12:03:57 +0200

    Add Readme file

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # passport-hubspot
 Passport strategy for authenticating with HubSpot using the OAuth 2.0 API.
+
+## Usage
+
+#### Configure Strategy
+
+    passport.use(new HubSpotStrategy({
+        clientID: HUBSPOT_APP_ID,
+        clientSecret: HUBSPOT_APP__SECRET,
+        callbackURL: "http://myapp.dev/hubspot/collback"
+      }, function(req, accessToken, refreshToken, profile, done) {
+        // Verify callback.
+      };
+    ));
+
+
+You can also define [Hubspot Scope](http://developers.hubspot.com/docs/methods/oauth2/initiate-oauth-integration) while configuring your Strategy. 
+
+
+    passport.use(new HubSpotStrategy({
+        clientID: HUBSPOT_APP_ID,
+        clientSecret: HUBSPOT_APP__SECRET,
+        callbackURL: "http://myapp.dev/hubspot/collback"
+        scope: [contacts]
+      }, function(req, accessToken, refreshToken, profile, done) {
+        // Verify callback.
+      };
+    ));
+    
+    
+** By default your Strategy is built based on those Scopes ['contacts', 'content', 'reports', 'social', 'automation', 'timeline'];
+
+## License
+[The MIT License](http://opensource.org/licenses/MIT)
--- strategy.js
+++ strategy.js
@@ -1,21 +1,19 @@
-/**
- * Module dependencies.
- */
 var OAuth2Strategy = require('passport-oauth2').Strategy
   , util = require('util');
 
 
 function Strategy(options, verify) {
-	
-	options = options || {};
-  	options.authorizationURL = options.authorizationURL || 'https://app.hubspot.com/oauth/authorize';
-  	options.tokenURL = options.tokenURL || 'https://api.hubapi.com/oauth/v1/token';
 
-  	OAuth2Strategy.call(this, options, verify);
+    options = options || {};
+    options.authorizationURL = options.authorizationURL || 'https://app.hubspot.com/oauth/authorize';
+    options.tokenURL = options.tokenURL || 'https://api.hubapi.com/oauth/v1/token';
+    options.scope = options.scope || ['contacts', 'content', 'reports', 'social', 'automation', 'timeline'];
 
-  	this.name = 'hubspot';
-  	this._oauth2._useAuthorizationHeaderForGET = true;
-  	this._scope = ['contacts', 'content', 'reports', 'social', 'automation', 'timeline'];
+    OAuth2Strategy.call(this, options, verify);
+
+    this.name = 'hubspot';
+    this._oauth2._useAuthorizationHeaderForGET = true;
+    this._scope = scope
 }
 
 util.inherits(Strategy, OAuth2Strategy);
